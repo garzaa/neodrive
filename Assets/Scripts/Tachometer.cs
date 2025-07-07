86e105c7dfd3a5eead260f554b31f5f9a6c178b0
Adrian Garza <adrianhgarza@gmail.com>
2025-07-06 17:55:23 -0700

    tachometer

--- Assets/Scripts/Tachometer.cs
+++ Assets/Scripts/Tachometer.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Tachometer : MonoBehaviour {
 	public RectTransform tachNeedle;
-    const float tachResting = 126 * Mathf.Deg2Rad;
+    const float tachResting = 0;
 
 	float rpmThisStep, rpmLastStep, delta;
 	float redline;
-	float offset;
 	float predictedRPM;
 	float rpmChangeSpeed;
 
-	const float tachRange = Mathf.PI * 1.2f;
+	const float tachRange = Mathf.PI * 1.5f;
+
+	public Image needle;
+	public Image needleBlur;
+
+	float speedFraction;
+
+	Color c;
 
 	public void SetRPM(float rpm, float engineRedline) {
 		redline = engineRedline;
@@ -20,12 +27,22 @@ public class Tachometer : MonoBehaviour {
 		rpmThisStep = rpm;
 		delta = rpmThisStep - rpmLastStep;
 		float speed = delta / Time.fixedDeltaTime;
-		float rpmChangeSpeed = speed * Time.deltaTime;
+		rpmChangeSpeed = speed ;
 		predictedRPM = rpmThisStep;
 	}
 
 	void Update() {
-		predictedRPM += rpmChangeSpeed;
+		predictedRPM += rpmChangeSpeed * Time.deltaTime;
+		speedFraction = Mathf.Clamp(Mathf.Abs(rpmChangeSpeed) / 10000f, 0, 1);
+
+		c = needle.color;
+		c.a = 1-speedFraction;
+		needle.color = c;
+
+		c = needleBlur.color;
+		c.a = speedFraction;
+		needleBlur.color = c;
+
         tachNeedle.rotation = Quaternion.Euler(new Vector3(
 			0,
 			0, 
