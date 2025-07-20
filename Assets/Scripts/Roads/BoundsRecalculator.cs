ddbf069baa55dabbb8073054ed509f618e1146d7
Adrian Garza <adrianhgarza@gmail.com>
2025-07-20 14:06:44 -0700

    i fixed it

---
+++ Assets/Scripts/Roads/BoundsRecalculator.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoundsRecalculator : MonoBehaviour {
+	// on spline change, go through all mesh LODs in child and recalculate their bounds
+}
