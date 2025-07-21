a008a8c5eeee3d1910c2040680dbb0adf74bfc18
Adrian Garza <adrianhgarza@gmail.com>
2025-07-20 21:37:57 -0700

    streetlights

--- Assets/Scripts/Roads/BoundsRecalculator.cs
+++ Assets/Scripts/Roads/BoundsRecalculator.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 
 [ExecuteInEditMode]
 public class BoundsRecalculator : MonoBehaviour {
 	// on spline change, go through all mesh LODs in child and recalculate their bounds
 	void Start() {
+		FixBounds();
+	}
+
+	[Button("Fix Spline LODs")]
+	void FixBounds() {
 		foreach (LODGroup lod in GetComponentsInChildren<LODGroup>()) {
 			print("recalculating bounds for "+ lod.name);
 			lod.RecalculateBounds();
