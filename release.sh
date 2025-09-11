b01f0b16090b8c773c19dee1e11b24473010779a
Adrian Garza <adrianhgarza@gmail.com>
2025-09-11 00:39:28 -0700

    add git release to release script

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 alias butler="D:/Program\ Files/butler-windows-amd64/butler.exe"
 alias 7z="C:/Program\ Files/7-Zip/7z.exe"
+alias gh="C:/Program\ Files/GitHub\ CLI/gh.exe"
 
 function itchrelease() {
     for i in win-exe gnu-linux win32-exe osx; do
@@ -13,8 +14,15 @@ function zip() {
     done
 }
 
+function gitrelease() {
+    TAG=$(cat ProjectSettings/ProjectSettings.asset | grep bundleVersion | cut -d':' -f2 | xargs)
+    echo $TAG
+    gh release create $TAG Demos/zips/*.zip  --generate-notes
+}
+
 set -x
 itchrelease
-# zip
+zip
+gitrelease
 
 set +x
